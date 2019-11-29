3a1a9b322541cfbd112ea61f4bc54b983a893b99
Ruslan Ibragimov <ruslan@ibragimov.by>
2019-11-29 15:18:02 +0300

    Display update date on the page.

--- generate-currencies.js
+++ generate-currencies.js
@@ -12,10 +12,22 @@ http.get(`http://data.fixer.io/api/latest?access_key=${process.env.FIXER_IO_TOKE
         const data = JSON.parse(chunks.join(""));
         const allCurrencies = {...data.rates, [data.base]: 1};
         const indexTemplate = fs.readFileSync("./dist/index.template.html", {encoding: "UTF-8"});
-        const renderedTemplate = indexTemplate.replace(`{"EUR": 1, "USD": 2, "BYN": 3}`, JSON.stringify(allCurrencies));
+        const renderedTemplate = indexTemplate
+            .replace(`{"EUR": 1, "USD": 2, "BYN": 3}`, JSON.stringify(allCurrencies))
+            .replace("January 1, 1970", getBuildDate());
         fs.writeFileSync("./dist/index.html", renderedTemplate);
     });
 }).on("error", (err) => {
     console.log(`Error: ${err.message}`);
     process.exit(1);
 });
+
+function getBuildDate() {
+    const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    };
+    const today = new Date();
+    return today.toLocaleDateString("en-US", options);
+}
