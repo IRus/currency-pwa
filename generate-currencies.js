a588ae717a7d0e5c86d8cd73459a61d4391fad04
Ruslan Ibragimov <ruslan@ibragimov.by>
2021-05-22 03:15:07 +0300

    Generate to json

--- generate-currencies.js
+++ generate-currencies.js
@@ -11,11 +11,10 @@ http.get(`http://data.fixer.io/api/latest?access_key=${process.env.FIXER_IO_TOKE
     response.on("end", () => {
         const data = JSON.parse(chunks.join(""));
         const allCurrencies = {...data.rates, [data.base]: 1};
-        const indexTemplate = fs.readFileSync("./dist/index.template.html", {encoding: "UTF-8"});
-        const renderedTemplate = indexTemplate
-            .replace(`{"EUR": 1, "USD": 2, "BYN": 3}`, JSON.stringify(allCurrencies))
-            .replace("January 1, 1970", getBuildDate());
-        fs.writeFileSync("./dist/index.html", renderedTemplate);
+        fs.writeFileSync("./src/data.json", JSON.stringify({
+          fixer: allCurrencies,
+          updated: getBuildDate()
+        }));
     });
 }).on("error", (err) => {
     console.log(`Error: ${err.message}`);
